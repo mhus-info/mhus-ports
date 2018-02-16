22446aa07cf18d672ecd9fbc63951dd9ad8b2329
Mike Hummel <>
2018-02-16 18:41:35 +0100

    version 1.3.2

--- karaf-services-maven-plugin/src/main/java/org/apache/karaf/tooling/tracker/GenerateServiceMetadata.java
+++ karaf-services-maven-plugin/src/main/java/org/apache/karaf/tooling/tracker/GenerateServiceMetadata.java
@@ -53,187 +53,185 @@ import org.sonatype.plexus.build.incremental.BuildContext;
 @Mojo(name = "service-metadata-generate", defaultPhase = LifecyclePhase.PROCESS_CLASSES, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME, inheritByDefault = false)
 public class GenerateServiceMetadata extends AbstractMojo {
 
-    /**
-     * The maven project.
-     */
-    @Parameter(defaultValue = "${project}")
-    protected MavenProject project;
+	/**
+	 * The maven project.
+	 */
+	@Parameter(defaultValue = "${project}")
+	protected MavenProject project;
 
-    @Parameter(defaultValue="BNDExtension-Bundle-Activator")
-    protected String activatorProperty;
+	@Parameter(defaultValue = "BNDExtension-Bundle-Activator")
+	protected String activatorProperty;
 
-    @Parameter(defaultValue="BNDExtension-Require-Capability")
-    protected String requirementsProperty;
+	@Parameter(defaultValue = "BNDExtension-Require-Capability")
+	protected String requirementsProperty;
 
-    @Parameter(defaultValue="BNDExtension-Provide-Capability")
-    protected String capabilitiesProperty;
+	@Parameter(defaultValue = "BNDExtension-Provide-Capability")
+	protected String capabilitiesProperty;
 
-    @Parameter(defaultValue = "${project.build.directory}/generated/karaf-tracker")
-    protected String outputDirectory;
+	@Parameter(defaultValue = "${project.build.directory}/generated/karaf-tracker")
+	protected String outputDirectory;
 
-    @Parameter(defaultValue=".*")
-    protected String artifactInclude;
-    
-    /**
-     * The classloader to use for loading the commands.
-     * Can be "project" or "plugin"
-     */
-    @Parameter(defaultValue = "project")
-    protected String classLoader;
-    
-    @Component
-    private BuildContext buildContext;
+	@Parameter(defaultValue = ".*")
+	protected String artifactInclude;
 
-    public void execute() throws MojoExecutionException, MojoFailureException {
-    		getLog().info("START karaf-services-maven-plugin artifactInclude: " + artifactInclude);
+	/**
+	 * The classloader to use for loading the commands. Can be "project" or
+	 * "plugin"
+	 */
+	@Parameter(defaultValue = "project")
+	protected String classLoader;
 
-        try {
-            boolean addSourceDirectory = false;
+	@Component
+	private BuildContext buildContext;
 
-            StringBuilder requirements = new StringBuilder();
-            StringBuilder capabilities = new StringBuilder();
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		getLog().info("START karaf-services-maven-plugin artifactInclude: " + artifactInclude);
 
-            ClassFinder finder = createFinder(classLoader);
-            List<Class<?>> classes = finder.findAnnotatedClasses(Services.class);
+		try {
+			boolean addSourceDirectory = false;
 
-            List<Class<?>> activators = new ArrayList<>();
-            for (Class<?> clazz : classes) {
+			StringBuilder requirements = new StringBuilder();
+			StringBuilder capabilities = new StringBuilder();
 
-                URL classUrl = clazz.getClassLoader().getResource(clazz.getName().replace('.', '/') + ".class");
-                URL outputDirectoryUrl = new File(project.getBuild().getOutputDirectory()).toURI().toURL();
-                if (classUrl == null || !classUrl.getPath().startsWith(outputDirectoryUrl.getPath())) {
-                		getLog().info("  Ignoring " + classUrl);
-                    continue;
-                } else {
-                		getLog().info(" Found " + classUrl);
-                }
+			ClassFinder finder = createFinder(classLoader);
+			List<Class<?>> classes = finder.findAnnotatedClasses(Services.class);
 
-                if (BundleActivator.class.isAssignableFrom(clazz)) {
-                    activators.add(clazz);
-                }
+			List<Class<?>> activators = new ArrayList<>();
+			for (Class<?> clazz : classes) {
 
-                Properties props = new Properties();
-                Services services = clazz.getAnnotation(Services.class);
-                if (services != null) {
-                    for (RequireService req : services.requires()) {
-                        String fltWithClass = combine(req.filter(), "(objectClass=" + req.value().getName() + ")");
-                        addServiceReq(requirements, fltWithClass);
-                        props.setProperty(req.value().getName(), req.filter());
-                    }
-                    for (ProvideService cap : services.provides()) {
-                        addServiceCap(capabilities, cap);
-                    }
-                }
-                Managed managed = clazz.getAnnotation(Managed.class);
-                if (managed != null) {
-                    props.setProperty("pid", managed.value());
-                }
+				URL classUrl = clazz.getClassLoader().getResource(clazz.getName().replace('.', '/') + ".class");
+				URL outputDirectoryUrl = new File(project.getBuild().getOutputDirectory()).toURI().toURL();
+				if (classUrl == null || !classUrl.getPath().startsWith(outputDirectoryUrl.getPath())) {
+					getLog().info("  Ignoring " + classUrl);
+					continue;
+				} else {
+					getLog().info(" Found " + classUrl);
+				}
 
-                File file = new File(outputDirectory, "OSGI-INF/karaf-tracker/" + clazz.getName());
-                file.getParentFile().mkdirs();
-                try (OutputStream os = buildContext.newFileOutputStream(file)) {
-                    props.store(os, null);
-                }
-                addSourceDirectory = true;
-            }
+				if (BundleActivator.class.isAssignableFrom(clazz)) {
+					activators.add(clazz);
+				}
 
-            if (addSourceDirectory) {
-                Resource resource = new Resource();
-                resource.setDirectory(outputDirectory);
-                project.addResource(resource);
-            }
+				Properties props = new Properties();
+				Services services = clazz.getAnnotation(Services.class);
+				if (services != null) {
+					for (RequireService req : services.requires()) {
+						String fltWithClass = combine(req.filter(), "(objectClass=" + req.value().getName() + ")");
+						addServiceReq(requirements, fltWithClass);
+						props.setProperty(req.value().getName(), req.filter());
+					}
+					for (ProvideService cap : services.provides()) {
+						addServiceCap(capabilities, cap);
+					}
+				}
+				Managed managed = clazz.getAnnotation(Managed.class);
+				if (managed != null) {
+					props.setProperty("pid", managed.value());
+				}
 
-            project.getProperties().setProperty(requirementsProperty, requirements.toString());
-            project.getProperties().setProperty(capabilitiesProperty, capabilities.toString());
-            if (activators.size() == 1) {
-                project.getProperties().setProperty(activatorProperty, activators.get(0).getName());
-            }
-            project.getProperties().setProperty("BNDExtension-Private-Package", "org.apache.karaf.util.tracker");
-            project.getProperties().setProperty("BNDPrependExtension-Import-Package", "!org.apache.karaf.util.tracker.annotation");
+				File file = new File(outputDirectory, "OSGI-INF/karaf-tracker/" + clazz.getName());
+				file.getParentFile().mkdirs();
+				try (OutputStream os = buildContext.newFileOutputStream(file)) {
+					props.store(os, null);
+				}
+				addSourceDirectory = true;
+			}
 
-            List<Class<?>> services = finder.findAnnotatedClasses(Service.class);
-            Set<String> packages = new TreeSet<>();
-            for (Class<?> clazz : services) {
-            		getLog().info(" Found Service " + clazz);
-                packages.add(clazz.getPackage().getName());
-            }
-            if (!packages.isEmpty()) {
-                project.getProperties().setProperty("BNDExtension-Karaf-Commands", join(packages, ","));
-            }
+			if (addSourceDirectory) {
+				Resource resource = new Resource();
+				resource.setDirectory(outputDirectory);
+				project.addResource(resource);
+			}
 
+			project.getProperties().setProperty(requirementsProperty, requirements.toString());
+			project.getProperties().setProperty(capabilitiesProperty, capabilities.toString());
+			if (activators.size() == 1) {
+				project.getProperties().setProperty(activatorProperty, activators.get(0).getName());
+			}
+			project.getProperties().setProperty("BNDExtension-Private-Package", "org.apache.karaf.util.tracker");
+			project.getProperties().setProperty("BNDPrependExtension-Import-Package",
+			        "!org.apache.karaf.util.tracker.annotation");
 
-        } catch (Exception e) {
-            throw new MojoExecutionException("Error building commands help", e);
-        }
-        getLog().info("STOP karaf-services-maven-plugin");
-    }
+			List<Class<?>> services = finder.findAnnotatedClasses(Service.class);
+			Set<String> packages = new TreeSet<>();
+			for (Class<?> clazz : services) {
+				getLog().info(" Found Service " + clazz);
+				packages.add(clazz.getPackage().getName());
+			}
+			if (!packages.isEmpty()) {
+				project.getProperties().setProperty("BNDExtension-Karaf-Commands", join(packages, ","));
+			}
 
-    private String join(Set<String> packages, String separator) {
-        StringBuilder sb = new StringBuilder();
-        for (String pkg : packages) {
-            if (sb.length() > 0) {
-                sb.append(separator);
-            }
-            sb.append(pkg);
-        }
-        return sb.toString();
-    }
+		} catch (Exception e) {
+			throw new MojoExecutionException("Error building commands help", e);
+		}
+		getLog().info("STOP karaf-services-maven-plugin");
+	}
 
-    private void addServiceCap(StringBuilder capabilities, ProvideService cap) {
-        if (capabilities.length() > 0) {
-            capabilities.append(",");
-        }
-        capabilities.append("osgi.service;effective:=active;objectClass=")
-                    .append(cap.value().getName());
-    }
+	private String join(Set<String> packages, String separator) {
+		StringBuilder sb = new StringBuilder();
+		for (String pkg : packages) {
+			if (sb.length() > 0) {
+				sb.append(separator);
+			}
+			sb.append(pkg);
+		}
+		return sb.toString();
+	}
 
-    private void addServiceReq(StringBuilder requirements, String fltWithClass) {
-        if (requirements.length() > 0) {
-            requirements.append(",");
-        }
-        requirements.append("osgi.service;effective:=active;filter:=\"")
-                    .append(fltWithClass)
-                    .append("\"");
-    }
+	private void addServiceCap(StringBuilder capabilities, ProvideService cap) {
+		if (capabilities.length() > 0) {
+			capabilities.append(",");
+		}
+		capabilities.append("osgi.service;effective:=active;objectClass=").append(cap.value().getName());
+	}
 
-    private String combine(String filter1, String filter2) {
-        if (filter1!=null && !filter1.isEmpty()) {
-            return "(&" + filter2 + filter1 + ")";
-        } else {
-            return filter2;
-        }
-    }
+	private void addServiceReq(StringBuilder requirements, String fltWithClass) {
+		if (requirements.length() > 0) {
+			requirements.append(",");
+		}
+		requirements.append("osgi.service;effective:=active;filter:=\"").append(fltWithClass).append("\"");
+	}
 
-    private ClassFinder createFinder(String classloaderType) throws Exception {
-        ClassFinder finder;
-        if ("project".equals(classloaderType)) {
-            List<URL> urls = new ArrayList<>();
+	private String combine(String filter1, String filter2) {
+		if (filter1 != null && !filter1.isEmpty()) {
+			return "(&" + filter2 + filter1 + ")";
+		} else {
+			return filter2;
+		}
+	}
 
-            urls.add( new File(project.getBuild().getOutputDirectory()).toURI().toURL() );
-            
-            for ( Artifact artifact : project.getArtifacts() ) {
-            		if (artifactInclude != null && artifactInclude.length() > 0 && artifact.getArtifactId().matches(artifactInclude)) {
-	                getLog().info(" Artifact " + artifact.getGroupId() + "/" + artifact.getArtifactId() );
-	
-	                File file = artifact.getFile();
-	                if ( file != null ) {
-	                    urls.add( file.toURI().toURL() );
-	                    getLog().debug("   classpath: " + file);
-	                }
-            		} else {
-            			getLog().info(" Ignore Artifact " + artifact.getGroupId() + "/" + artifact.getArtifactId());
-            		}
-            }
-            
-            ClassLoader loader = new URLClassLoader(urls.toArray(new URL[urls.size()]), getClass().getClassLoader());
-            getLog().info("Load Classes; " + urls.size() + " jars");
-            finder = new ClassFinder(loader, urls);
-        } else if ("plugin".equals(classLoader)) {
-            finder = new ClassFinder(getClass().getClassLoader());
-        } else {
-            throw new MojoFailureException("classLoader attribute must be 'project' or 'plugin'");
-        }
-        return finder;
-    }
+	private ClassFinder createFinder(String classloaderType) throws Exception {
+		ClassFinder finder;
+		if ("project".equals(classloaderType)) {
+			List<URL> urls = new ArrayList<>();
+
+			urls.add(new File(project.getBuild().getOutputDirectory()).toURI().toURL());
+
+			for (Artifact artifact : project.getArtifacts()) {
+				if (artifactInclude != null && artifactInclude.length() > 0
+				        && artifact.getArtifactId().matches(artifactInclude)) {
+					getLog().info(" Artifact " + artifact.getGroupId() + "/" + artifact.getArtifactId());
+
+					File file = artifact.getFile();
+					if (file != null) {
+						urls.add(file.toURI().toURL());
+						getLog().debug("   classpath: " + file);
+					}
+				} else {
+					getLog().info(" Ignore Artifact " + artifact.getGroupId() + "/" + artifact.getArtifactId());
+				}
+			}
+
+			ClassLoader loader = new URLClassLoader(urls.toArray(new URL[urls.size()]), getClass().getClassLoader());
+			getLog().info("Load Classes; " + urls.size() + " jars");
+			finder = new ClassFinder(loader, urls);
+		} else if ("plugin".equals(classLoader)) {
+			finder = new ClassFinder(getClass().getClassLoader());
+		} else {
+			throw new MojoFailureException("classLoader attribute must be 'project' or 'plugin'");
+		}
+		return finder;
+	}
 
 }
