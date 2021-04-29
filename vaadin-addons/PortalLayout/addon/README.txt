e53bdb355adaac04c0a54e469de0f68bc5ed712c
Mike Hummel <mike.hummel@billingsolutions.de>
2021-04-29 15:24:23 +0200

    remove deprecated, fix vaadin versions

--- vaadin-addons/PortalLayout/addon/README.txt
+++
@@ -1,3 +0,0 @@
-Server-side parts of an add-on, packaged as a JAR.
-
-https://github.com/p4elkin/PortalLayout/pull/7/files#diff-b55b806510b0c501f8ecfddc9383f648
--- vaadin-addons/PortalLayout/demo/src/main/webapp/VAADIN/widgetsets/org.vaadin.addons.portallayout.gwt.PortalLayoutDemoWidgetSet/deferredjs/8ACD3F3BDAE0C9F1C087718EC8E9DCF7/1.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.org_vaadin_addons_portallayout_gwt_PortalLayoutDemoWidgetSet.runAsyncCallback1('function $load_1(){\n}\n\ndefineSeed(2563, 1, makeCastMap([Q$RunAsyncCallback]));\n_.onSuccess_1 = function onSuccess_2(){\n  $load_1();\n  $setLoaded_0(get_13(), $getName_2(this.this$1));\n}\n;\n$entry(onLoad)(1);\n\n//@ sourceURL=1.js\n')
